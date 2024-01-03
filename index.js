b4ee6d77ed1020eff3df3ac8ea0c07612bc61794
thomazfp <thomaz.fp@gmail.com>
2024-01-03 18:46:08 -0300

    Repositório DIO

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("hello")
+let nomeDoHeroi = "Ashgard";
+let xpDoHeroi = 10200;
+
+let nivelDoHeroi;
+
+if (xpDoHeroi < 1000) {
+    nivelDoHeroi = "Ferro";
+} else if (xpDoHeroi >= 1001 && xpDoHeroi <= 2000) {
+    nivelDoHeroi = "Bronze";
+} else if (xpDoHeroi >= 2001 && xpDoHeroi <= 5000) {
+    nivelDoHeroi = "Prata";
+} else if (xpDoHeroi >= 5001 && xpDoHeroi <= 7000) {
+    nivelDoHeroi = "Ouro";
+} else if (xpDoHeroi >= 7001 && xpDoHeroi <= 8000) {
+    nivelDoHeroi = "Platina";
+} else if (xpDoHeroi >= 8001 && xpDoHeroi <= 9000) {
+    nivelDoHeroi = "Ascendente";
+} else if (xpDoHeroi >= 9001 && xpDoHeroi <= 10000) {
+    nivelDoHeroi = "Imortal";
+} else {
+    nivelDoHeroi = "Radiante";
+}
+
+console.log(nomeDoHeroi + " está no nível " + nivelDoHeroi)
+
